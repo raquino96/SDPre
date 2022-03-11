1424c402a1924e62f9f96ea80ddf207ce2481d33
raquino96 <ronnieaquino4@gmail.com>
2022-03-11 01:36:16 -0500

    complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,11 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 4;
+    num2 = 12;
+    num3 = 24;
+    average = (num1 + num2 + num3)/3;
+
+    System.out.println(average);
+  }
+}
