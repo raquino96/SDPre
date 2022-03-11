2d0ba60c36354311c21ed40bb08407815c9f56a3
raquino96 <ronnieaquino4@gmail.com>
2022-03-11 00:59:34 -0500

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Ronnie Aquino";
+    int age = 35;
+    String state = "Maryland";
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
