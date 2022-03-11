cd1655b7913e6fa1ba4276f5111ed4278b8a74dd
raquino96 <ronnieaquino4@gmail.com>
2022-03-11 01:06:56 -0500

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 2;
+    num2 = 6;
+    num3 = 12;
+    average = (num1 + num2 + num3)/3;
+
+    System.out.println(average);
+  }
+}
