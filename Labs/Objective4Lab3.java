2b5fded86c576a2d2be792c76589b2df2fd152e6
raquino96 <ronnieaquino4@gmail.com>
2022-03-13 16:23:21 -0400

    complete Objective4Lab3 (using YearMonth utility)

--- Labs/Objective4Lab3.java
+++ Labs/Objective4Lab3.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
+import java.time.YearMonth;
 
 public class Objective4Lab3 {
   public static void main (String[] args) {
-    int birthYear, age, currentYear;
+    int birthYear, age;
 
     birthYear = 0;
+    int currentYear = YearMonth.now().getYear();
 
     Scanner input = new Scanner(System.in);
 
     System.out.println("How old are you? ");
     age = input.nextInt();
-    System.out.println("What year is it currently? ");
-    currentYear = input.nextInt();
 
     birthYear = currentYear - age;
 
     System.out.println("You were born in " + birthYear);
-
-
   }
 }
