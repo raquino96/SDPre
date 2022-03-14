8a6576af10e7c693c140a0bcf7db05be187e89c9
raquino96 <ronnieaquino4@gmail.com>
2022-03-13 22:02:58 -0400

    complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int counter = 1;
+    int sumTotal = 0;
+
+    while(counter <=20) {
+      sumTotal += counter;
+      counter ++;
+    }
+    System.out.println(sumTotal);
+  }
+}
