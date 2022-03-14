0401b96a78c70baafecc2fb561f9c66dc32083d2
raquino96 <ronnieaquino4@gmail.com>
2022-03-13 22:55:54 -0400

    complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int sumTotal = 0;
+    int count = 0;
+
+    for(count = 1; count <= 20; count ++) {
+      sumTotal += count;
+    }
+    System.out.println(sumTotal);
+  }
+}
