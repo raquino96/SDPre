3c32b42e0e059cf61696b1400f310a5595ef1bdb
raquino96 <ronnieaquino4@gmail.com>
2022-03-13 22:49:11 -0400

    complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    int numIncrease, numDecrease;
+
+    for(numIncrease = 1; numIncrease < 11; numIncrease++) {
+      System.out.println(numIncrease);
+    }
+
+    for(numDecrease = 10; numDecrease > 0; numDecrease --) {
+    System.out.println(numDecrease);
+    }
+  }
+}
